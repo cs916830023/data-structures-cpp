8f58c968b6eb3967162a1601987f89a1be5a2769
chensong <chens@zhongyitech.com>
2023-09-25 10:30:10 +0800

    111

---
+++ Array/obj/local/x86_64/objs/Array/__/src/main.o.d
@@ -0,0 +1,198 @@
+./obj/local/x86_64/objs/Array/__/src/main.o: jni/../src/main.cpp \
+  jni/../src/test.h jni/../src/sparse_matrix.h \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/stdexcept \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/__config \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/exception \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/cstddef \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/version \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/cstdlib \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/stdlib.h \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/type_traits \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/iosfwd \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/wchar.h \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/iostream \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/ios \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/__locale \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/string \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/string_view \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/__string \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/algorithm \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/initializer_list \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/cstring \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/string.h \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/utility \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/__tuple \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/cstdint \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/stdint.h \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/__debug \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/memory \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/typeinfo \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/new \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/limits \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/iterator \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/__functional_base \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/tuple \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/atomic \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/__threading_support \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/chrono \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/ctime \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/ratio \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/climits \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/limits.h \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/errno.h \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/functional \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/bit \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/cstdio \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/stdio.h \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/cwchar \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/cwctype \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/cctype \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/ctype.h \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/wctype.h \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/mutex \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/__mutex_base \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/system_error \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/__errc \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/cerrno \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/locale.h \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/support/android/locale_bionic.h \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/streambuf \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/istream \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/ostream \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/locale \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/cstdarg \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/__bsd_locale_fallbacks.h \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/bitset \
+  /opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/__bit_reference
+
+jni/../src/test.h:
+
+jni/../src/sparse_matrix.h:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/stdexcept:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/__config:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/exception:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/cstddef:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/version:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/cstdlib:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/stdlib.h:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/type_traits:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/iosfwd:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/wchar.h:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/iostream:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/ios:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/__locale:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/string:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/string_view:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/__string:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/algorithm:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/initializer_list:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/cstring:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/string.h:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/utility:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/__tuple:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/cstdint:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/stdint.h:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/__debug:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/memory:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/typeinfo:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/new:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/limits:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/iterator:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/__functional_base:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/tuple:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/atomic:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/__threading_support:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/chrono:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/ctime:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/ratio:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/climits:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/limits.h:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/errno.h:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/functional:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/bit:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/cstdio:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/stdio.h:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/cwchar:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/cwctype:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/cctype:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/ctype.h:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/wctype.h:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/mutex:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/__mutex_base:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/system_error:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/__errc:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/cerrno:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/locale.h:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/support/android/locale_bionic.h:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/streambuf:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/istream:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/ostream:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/locale:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/cstdarg:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/__bsd_locale_fallbacks.h:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/bitset:
+
+/opt/homebrew/Caskroom/android-ndk/25c/AndroidNDK9519653.app/Contents/NDK/sources/cxx-stl/llvm-libc++/include/__bit_reference:
